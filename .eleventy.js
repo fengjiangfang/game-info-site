81dd1d1381a6e3fdf2e79ee4f8458bc4ee7dc143
fengjiangfang <fengjiangfang@gmail.com>
2026-03-29 05:08:40 +0800

    UI: 優化稱號獎勵文字行高，達成視覺垂直對齊

--- .eleventy.js
+++ .eleventy.js
@@ -5,18 +5,11 @@ module.exports = function(eleventyConfig) {
     eleventyConfig.addPassthroughCopy("src/css");
 
     // 配置
-    // 偵測是否為本地開發伺服器模式
-    const isLocalServer = process.argv.includes('--serve');
-
     return {
         dir: {
             input: "src",
             output: "dist"
         },
-        // 統一鎖定路徑前綴，確保本地與線上環境路徑結構一致，徹底移除 404 風險
-        pathPrefix: "/game-info-site/",
-        templateFormats: ["njk", "md", "html"],
-        htmlTemplateEngine: "njk",
-        markdownTemplateEngine: "njk"
+        pathPrefix: "/game-info-site/"
     };
 };
